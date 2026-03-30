1d43d3e4a21762329619141dec99a35007585f81
KAUAVINICIOS0 <kaua.vinicios_2005@hotmail.com>
2026-03-30 15:37:11 -0300

    feat: implements delete method in application

--- cars/urls.py
+++ cars/urls.py
@@ -5,5 +5,6 @@ app_name = 'cars'
 
 urlpatterns = [
     path("", views.get_cars, name="index"),
-    path("create/", views.post_car, name='create_car')
+    path("create/", views.post_car, name='create_car'),
+    path("delete/<int:id>", views.delete_car, name='delete_car'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Car
 from .forms import CarForm
@@ -11,7 +11,7 @@ def get_cars(request):
     return render(request, 'cars/index.html', {'cars': cars})
 
 
-def post_car(request):
+def post_car(request:HttpRequest):
     if request.method == 'POST':
         form = CarForm(request.POST)
         if form.is_valid():
@@ -21,3 +21,8 @@ def post_car(request):
         'form': CarForm()
     }
     return render(request, 'cars/create.html', context)
+
+def delete_car(request:HttpRequest, id):
+    car = get_object_or_404(Car, id=id)
+    car.delete()
+    return redirect('cars:index')
