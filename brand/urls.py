63cc1d74cef9ab8b62aa551a08303120497e16ac
KAUAVINICIOS0 <kaua.vinicios_2005@hotmail.com>
2026-03-31 15:41:05 -0300

    feat: implements Delete brand in model

--- brand/urls.py
+++ brand/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'brands'
 
 urlpatterns  = [
-    path('', views.get_brands, name='index')
+    path('', views.get_brands, name='index'),
+    path('create/', views.post_brand, name='create_brand'),
+    path('delete/<int:id>', views.delete_brand, name='delete_brand'),
+
 ]
--- brand/views.py
+++ brand/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Brand
+from .forms import BrandForm
+from django.http import HttpResponse
+from django.http import HttpRequest
 # Create your views here.
 
 
 def get_brands(request):
     brands = Brand.objects.all()
     return render(request,'brand/index.html', {'brands':brands})
+
+
+def post_brand(request:HttpRequest):
+    if request.method == 'POST':
+        form = BrandForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('brands:index')
+    context = {
+        'form': BrandForm()
+    }
+    return render(request, 'brand/create.html', context)
+
+def delete_brand(request:HttpRequest, id):
+    brand = get_object_or_404(Brand, id=id)
+    brand.delete()
+    return redirect('brands:index')
