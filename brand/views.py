a560489b6b307eeb77f9e0cd14d91c117bb6fbff
KAUAVINICIOS0 <kaua.vinicios_2005@hotmail.com>
2026-03-31 12:48:02 -0300

    feat: implements model call brand in car model, and implements get in model

--- brand/views.py
+++ brand/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Brand
 # Create your views here.
+
+
+def get_brands(request):
+    brands = Brand.objects.all()
+    return render(request,'brand/index.html', {'brands':brands})
--- cars/models.py
+++ cars/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-
+from brand.models import Brand
 # Create your models here.
 
 class Car(models.Model):
     name = models.CharField(max_length=100)
     year = models.IntegerField(null=True, blank=True)
     color = models.CharField(max_length=50, null=True, blank=True)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='cars', null=True)
     data_created = models.DateTimeField(auto_now_add=True)
     data_updated = models.DateTimeField(auto_now=True)
 
